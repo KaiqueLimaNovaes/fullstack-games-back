6f79c8ed746b1ffb30f9fd5069c9e55f0dd2d7a7
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2020-12-17 18:47:07 -0300

    Att conexão servido 8

--- Connection.php
+++ Connection.php
@@ -7,7 +7,7 @@ class Connection
         $conn = new PDO(
             "mysql:host=192.168.0.41;dbname=fullstackgames",
             "kaique",
-            "12345"
+            ""
         );
 
         if ($conn) {

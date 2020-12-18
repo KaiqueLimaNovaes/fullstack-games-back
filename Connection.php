7106b563931b31e144194f423e107b8f94677cc9
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2020-12-18 09:24:35 -0300

    Att 11

--- Connection.php
+++ Connection.php
@@ -4,6 +4,7 @@ class Connection
 {
     public static function getDb()
     {
+        echo "<h1>Teste</h1>";
         $conn = new PDO(
             "mysql:host=192.168.0.41;dbname=fullstackgames",
             "kaique",
